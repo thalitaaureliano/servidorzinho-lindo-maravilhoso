ddfd9c8cac3d54fe22c98c1886ac0b2e9d458145
Josélia L. Costa <joselialcosta@gmail.com>
2019-05-28 22:15:57 -0300

    Cria nosso primeiro servidor com nodemon e duas rotas

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 const http = require('http')
+
+const servidor = http
+  .createServer(function (request, response) {
+    if (request.url === '/') {
+      response.end('Hello wonderful world!')
+    } else if (request.url === '/comidas') {
+      if (request.method === 'GET') {
+        response.writeHead(200, {
+          "Content-Type": "text/html;charset=utf-8"
+        })
+        response.end("<h1>Respostão diferentona</h1>")
+      } else if (request.method === 'POST') {
+        response.writeHead(201, {
+          "Content-Type": "text/html;charset=utf-8"
+        })
+        response.end("<h1>Respostão diferentona do post</h1>")
+      }
+    }
+  })
+
+servidor.listen(3000)
+console.log("servidorzinho rodando na porta 3000")
